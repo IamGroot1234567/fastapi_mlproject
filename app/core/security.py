445ca959d904b352dd9673382943a947b208e65c
groot <groot@gmail.com>
2026-09-13 15:24:50 +0530

    added app configurations

---
+++ app/core/security.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta, timezone
+
+from jose import JWTError, jwt
+
+from config import settings
+
+
+def create_token(data: dict, expire_minutes=30):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=expire_minutes)
+    to_encode.update({'exp': expire})
+    token = jwt.encode(
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
+
+    return token
+
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM)
+        return payload
+    except JWTError:
+        return None
