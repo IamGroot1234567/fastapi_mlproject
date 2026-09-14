6aecc6f051dce4f0e1496d92af163637f545350e
groot <groot@gmail.com>
2026-09-14 18:40:07 +0530

    modified core

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
+("Any unhandled exception from any endpoint in this FastAPI application should be handled by this "
+ "global exception handler.")
+
 
 def register_exception_handler(app: FastAPI):
     @app.add_exception_handler(Exception)
     async def unhandled_exception_handler(request: Request, exc: Exception):
-        return JSONResponse(status_code=500, content={'det ail': str(exc)})
+        return JSONResponse(
+            status_code=500,
+            content={'detail': str(exc)}
+        )
